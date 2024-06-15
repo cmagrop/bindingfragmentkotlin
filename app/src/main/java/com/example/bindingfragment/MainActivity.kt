f3d2a5f446af6629d5ca103894515ce249b1ce57
=Christian Magro Pérez <cmagrop@gmail.com>
2024-06-15 12:24:09 -0400

    modificacion de strings.xml, activity_main2.xml y MainActivity.kt

--- app/src/main/java/com/example/bindingfragment/MainActivity.kt
+++ app/src/main/java/com/example/bindingfragment/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.bindingfragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bindingfragment.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { //class MainActivity extends AppCompatActivity
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root) //definir el layout raiz activity_main LinearLayout
+        //vincular el boton
+        binding.entrarButton.setOnClickListener {
+            //cuerpo del metodo setOnClickLister
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
         }
+
+
     }
 }
