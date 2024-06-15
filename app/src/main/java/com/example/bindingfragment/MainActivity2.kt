f9ff7dafda184dff1036e19f8de3d756a1626e55
=Christian Magro Pérez <cmagrop@gmail.com>
2024-06-15 12:45:08 -0400

    Se actualizó fragment_chat.xml, fragment_home.xml y MainActivivity2.kt

--- app/src/main/java/com/example/bindingfragment/MainActivity2.kt
+++ app/src/main/java/com/example/bindingfragment/MainActivity2.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bindingfragment.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var binding = ActivityMain2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.home.setOnClickListener {
+
+            //vamos a implementar el fragment de Home
+
         }
+
+        binding.chat.setOnClickListener {
+
+            //vamos a implementar el fragment de Chat
+        }
+
     }
 }
