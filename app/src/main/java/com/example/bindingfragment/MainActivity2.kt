01f99595bdabd0e294799762dfc8d9a4589a060b
=Christian Magro Pérez <cmagrop@gmail.com>
2024-06-17 20:00:39 -0400

    modificaciones finales de fragments y MainActivity2

--- app/src/main/java/com/example/bindingfragment/MainActivity2.kt
+++ app/src/main/java/com/example/bindingfragment/MainActivity2.kt
@@ -1,11 +1,15 @@
 package com.example.bindingfragment
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.bindingfragment.databinding.ActivityMain2Binding
+import com.example.bindingfragment.fragments.ChatFragment
+import com.example.bindingfragment.fragments.HomeFragment
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
@@ -18,13 +22,28 @@ class MainActivity2 : AppCompatActivity() {
         binding.home.setOnClickListener {
 
             //vamos a implementar el fragment de Home
+            replaceFragment(HomeFragment())
+            Toast.makeText(this,"Home",Toast.LENGTH_LONG).show()
+
 
         }
 
         binding.chat.setOnClickListener {
 
             //vamos a implementar el fragment de Chat
+            replaceFragment(ChatFragment())
+            Toast.makeText(this,"Chat",Toast.LENGTH_LONG).show()
         }
 
     }
+
+    fun replaceFragment(fragment:Fragment)
+    {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment,fragment)
+        //replace(fragmento a reemplazar en activity_main2.xml,fragmento de reemplazo)
+        fragmentTransaction.commit()
+
+    }
+
 }
